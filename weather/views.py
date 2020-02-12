e74456fe2ec9f23e74dc1de5c82c2378f902bff3
Paul Luna <paulluna0215@gmail.com>
2020-02-12 12:42:26 -0800

    added API and getting real time data

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather/index.html') # returns the index.html template
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=19b3b8c0da1d8b61cf8280b923446727'
+    city = 'San Diego'
+    city_weather = requests.get(url.format(city)).json() # request the API data and convert the JSON to Python data types
+
+    weather = {
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+
+    context = {
+        'weather': weather
+    }
+    return render(request, 'weather/index.html', context) # returns the index.html template
