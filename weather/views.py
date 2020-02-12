9121690d3428f5ce5d1383f25444d5e54f7f0fc6
Paul Luna <paulluna0215@gmail.com>
2020-02-12 13:37:56 -0800

    user can now add city to the database from user interface

--- weather/views.py
+++ weather/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
+
 import requests
+
 from .models import City
+from .forms import CityForm
+
 
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=19b3b8c0da1d8b61cf8280b923446727'
     cities = City.objects.all() # return all the cities in the database
 
-    city = 'San Diego'
-    city_weather = requests.get(url.format(city)).json() # request the API data and convert the JSON to Python data types
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save()
+
+    form = CityForm()
     weather_data = []
 
     for city in cities:
         city_weather = requests.get(url.format(city.name)).json() # request the API data and convert the JSON to Python data types
-        print(city_weather)
-        print(city.name)
         weather  = {
             'city': city,
             'temperature': city_weather['main']['temp'],
@@ -25,6 +30,7 @@ def index(request):
         weather_data.append(weather) # add data of our current city to our list
 
     context = {
-        'weather_data': weather_data
+        'weather_data': weather_data,
+        'form': form
     }
     return render(request, 'weather/index.html', context) # returns the index.html template
